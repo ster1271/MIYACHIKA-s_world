c068d1e2dc66d0319f20cb350c747cee035a9db2
okoking <144639739+okoking@users.noreply.github.com>
2025-07-11 15:26:06 +0900

    Result

--- src/SceneManager/SceneClear/SceneClear.h
+++ src/SceneManager/SceneClear/SceneClear.h
@@ -1,4 +1,5 @@
 #pragma once
+#pragma once
 #include "../SceneBace/SceneBace.h"
 #include "../../Common.h"
 
@@ -8,20 +9,23 @@ public:
 	// 表示する画像の種類
 	enum GRAPH_TYPE {
 		GRAPH_TYPE_BG,
-		GRAPH_TYPE_BLOCK,
-		GRAPH_TYPE_THORN,
+		GRAPH_TYPE_BAR,
+		GRAPH_TYPE_JUMP,
 		GRAPH_TYPE_GOAL,
+		GRAPH_TYPE_DEAD,
 		GRAPH_TYPE_NUM,
+	};
 
+	// 表示するスコアの種類
+	enum SCORE_TYPE {
+		SCORE_TYPE_JUMP,
+		SCORE_TYPE_CLEAR,
+		SCORE_TYPE_DEAD,
+		SCORE_TYPE_TOTAL,
+		SCORE_TYPE_NUM,
 	};
 private:
-	struct Goal {
-		int	m_Pos[2];
-	};
-
 	int m_Hndl[GRAPH_TYPE_NUM];	// 画像ハンドル
-
-	vector<Goal> m_GoalVec;
 public:
 	void Init();
 	void Step();
